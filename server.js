ccd5aac5aa8fccef52cdf90bd459ebc84499338a
Hirunika <hirukarunathilaka@gmail.com>
2019-08-29 18:22:29 +0530

    user registration

--- server.js
+++ server.js
@@ -1,8 +1,40 @@
-const http = require('http');
-const app = require('./app');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const port = process.env.PORT || 3000 ;
+// create express app
+const app = express();
 
-const server = http.createServer(app);
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
 
-server.listen(port);
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+//connecting to db
+mongoose.connect(dbConfig.url,{
+    useNewUrlParser:true
+}).then(()=>{
+    console.log("successfully connected to db");
+}).catch(err => {
+    console.log("Could not connect to db");
+    process.exit();
+})
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to Vid-talk"});
+});
+
+//Require Users routes
+require('./app/routes/user.routes.js')(app);
+
+// listen for requests
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
